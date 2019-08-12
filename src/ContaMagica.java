2d1f90be4a62638521e3604ee505631a0a6510c2
Matheus Inhaia <matheus.inhaia2014@gmail.com>
2019-08-12 16:33:38 -0300

    implementando a classe contaMagica, contamagica(), getNome(), getSaldo(), getStatus(), deposito(), retirada().

--- src/ContaMagica.java
+++ src/ContaMagica.java
@@ -1,2 +1,43 @@
+import java.math.BigDecimal;
+
 public class ContaMagica {
+
+    public enum Categoria{
+        Silver, Gold, Platinum;
+    }
+    private Categoria categorias;
+    private String cliente;
+    private BigDecimal saldo;
+
+    public ContaMagica(String nome){
+        this.cliente = nome;
+        this.categorias = Categoria.Silver;
+        this.saldo = new BigDecimal(0);
+    }
+    public String getNomeCliente(){
+        return this.cliente;
+    }
+    public BigDecimal getSaldo(){
+        return this.saldo;
+    }
+    public Categoria getStatus(){
+        return this.categorias;
+    }
+
+
+    public void deposito(BigDecimal valor) {
+        if(valor.compareTo(new BigDecimal(0))== 1){   //new BigDecimal(0).compareTo(valor)== 1){
+            saldo.add(valor);
+        }else{
+            throw new IllegalArgumentException("valor invalido");
+        }
+    }
+
+    public void retirada(BigDecimal valor){
+        if(valor.compareTo(saldo) == -1 || valor.compareTo(saldo) == 0){
+             saldo.subtract(valor);
+        }else{
+            throw new IllegalArgumentException("valor excede o saldo");
+        }
+    }
 }
